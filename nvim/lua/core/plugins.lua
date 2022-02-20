e1527de953a0de7b5c13903b2df81946ef8f2a1e
Humber Aquino <humberaquino@gmail.com>
2022-02-20 19:46:39 -0300

    Add lazygit

--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -91,4 +91,6 @@ return packer.startup(function(use)
 		"ruifm/gitlinker.nvim",
 		requires = "nvim-lua/plenary.nvim",
 	})
+
+	use("kdheepak/lazygit.nvim")
 end)
--- nvim/lua/plugin/whichkey-setup.lua
+++ nvim/lua/plugin/whichkey-setup.lua
@@ -114,7 +114,7 @@ local mappings = {
 			"<cmd>Gitsigns diffthis HEAD<cr>",
 			"Diff",
 		},
-		g = { "<cmd>lua _LAZYGIT_TOGGLE()<cr>", "Lazygit" },
+		g = { "<cmd>LazyGit<cr>", "Lazygit" },
 		j = { "<cmd>lua require 'gitsigns'.next_hunk()<cr>", "Next Hunk" },
 		k = { "<cmd>lua require 'gitsigns'.prev_hunk()<cr>", "Prev Hunk" },
 		l = { "<cmd>lua require 'gitsigns'.blame_line()<cr>", "Blame" },
