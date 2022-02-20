5fd327d0a8d58f72c5b126f5cabf756f213fba97
Humber Aquino <humberaquino@gmail.com>
2022-02-20 12:16:10 -0300

    Check treesitter before loading module

--- nvim/lua/plugin/treesitter-setup.lua
+++ nvim/lua/plugin/treesitter-setup.lua
@@ -1,17 +1,22 @@
-require 'nvim-treesitter.configs'.setup {
--- One of "all", "maintained" (parsers with maintainers), or a list of languages
-ensure_installed = {
-    'python',
-    'lua',
-    'javascript',
-    'elixir',
-},
+local status_ok, _ = pcall(require, "nvim-treesitter.configs")
+if not status_ok then
+	return
+end
 
--- Install languages synchronously (only applied to `ensure_installed`)
-sync_install = false,
+require("nvim-treesitter.configs").setup({
+	-- One of "all", "maintained" (parsers with maintainers), or a list of languages
+	ensure_installed = {
+		"python",
+		"lua",
+		"javascript",
+		"elixir",
+	},
 
-highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-},
-}
+	-- Install languages synchronously (only applied to `ensure_installed`)
+	sync_install = false,
+
+	highlight = {
+		enable = true,
+		additional_vim_regex_highlighting = false,
+	},
+})
