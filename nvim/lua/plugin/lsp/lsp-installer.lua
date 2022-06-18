320085bc424c63f809f253ab9cad8b72e3b7d5d1
Humberto Aquino <humberto@appcues.com>
2022-06-18 15:31:39 -0400

    Fix config after sync repos

--- nvim/lua/plugin/lsp/lsp-installer.lua
+++ nvim/lua/plugin/lsp/lsp-installer.lua
@@ -1,45 +1,49 @@
 local status_ok, lsp_installer = pcall(require, "nvim-lsp-installer")
 if not status_ok then
-	return
+  return
 end
 
 -- Include the servers you want to have installed by default below
 local servers = {
-	"bashls",
-	"pyright",
-	"elixirls",
-	"yamlls",
-	"sumneko_lua",
-	"jsonls",
+  "bashls",
+  "pyright",
+  "elixirls",
+  "yamlls",
+  "sumneko_lua",
+  "jsonls",
 }
 
 for _, name in pairs(servers) do
-	local server_is_found, server = lsp_installer.get_server(name)
-	if server_is_found and not server:is_installed() then
-		print("Installing " .. name)
-		server:install()
-	end
+  local server_is_found, server = lsp_installer.get_server(name)
+  if server_is_found and not server:is_installed() then
+    print("Installing " .. name)
+    server:install()
+  end
 end
 
 -- Register a handler that will be called for all installed servers.
 -- Alternatively, you may also register handlers on specific server instances instead (see example below).
 lsp_installer.on_server_ready(function(server)
-	local opts = {
-		on_attach = require("plugin.lsp.handlers").on_attach,
-		capabilities = require("plugin.lsp.handlers").capabilities,
-	}
+  local opts = {
+    on_attach = require("plugin.lsp.handlers").on_attach,
+    capabilities = require("plugin.lsp.handlers").capabilities,
+  }
 
-	-- if server.name == "jsonls" then
-	-- 	local jsonls_opts = require("plugin.lsp.settings.jsonls")
-	-- 	opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
-	-- end
+  -- if server.name == "jsonls" then
+  -- 	local jsonls_opts = require("plugin.lsp.settings.jsonls")
+  -- 	opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
 
-	-- if server.name == "sumneko_lua" then
-	-- 	local sumneko_opts = require("plugin.lsp.settings.sumneko_lua")
-	-- 	opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
-	-- end
+  -- if server.name == "elixirls" then
+  -- 	local elixir_opts= require("plugin.lsp.settings.elixirls")
+  -- 	opts = vim.tbl_deep_extend("force", elixir_opts, opts)
+  -- end
 
-	-- This setup() function is exactly the same as lspconfig's setup function.
-	-- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
-	server:setup(opts)
+  -- if server.name == "sumneko_lua" then
+  -- 	local sumneko_opts = require("plugin.lsp.settings.sumneko_lua")
+  -- 	opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
+  -- end
+
+  -- This setup() function is exactly the same as lspconfig's setup function.
+  -- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+  server:setup(opts)
 end)
--- nvim/lua/plugin/nvim-tree-setup.lua
+++ nvim/lua/plugin/nvim-tree-setup.lua
@@ -1,19 +1,24 @@
 local status_ok, nvimtree = pcall(require, "nvim-tree")
 if not status_ok then
-	return
+  return
 end
 
-vim.g.nvim_tree_respect_buf_cwd = 1
-vim.g.nvim_tree_highlight_opened_files = 1
-vim.g.nvim_tree_git_hl = 1
+-- vim.g.nvim_tree_respect_buf_cwd = 1
+-- vim.g.nvim_tree_highlight_opened_files = 1
+-- vim.g.nvim_tree_git_hl = 1
 
 nvimtree.setup({
-	update_cwd = true,
-	update_focused_file = {
-		enable = true,
-		update_cwd = true,
-	},
-	diagnostics = {
-		enable = true,
-	},
+  update_cwd = true,
+  update_focused_file = {
+    enable = true,
+    update_cwd = true,
+  },
+  diagnostics = {
+    enable = true,
+  },
+  renderer = {
+    highlight_git = true,
+    highlight_opened_files = "icon"
+  },
+  respect_buf_cwd = true
 })
